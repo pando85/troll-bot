857aa72d6e99490b7f4bc8f69fc8598dda16bf3d
Pando85 <pando855@gmail.com>
2016-03-24 01:12:07 +0100

    Refactored handler and reply

--- troll_bot/handler.py
+++ troll_bot/handler.py
@@ -3,8 +3,7 @@ import logging
 from telegram.dispatcher import run_async
 
 from troll_bot.database import save_message
-from troll_bot.reply import reply_message
-
+from troll_bot.reply import get_random_word, get_reply_message, should_reply, get_reply_type
 
 log = logging.getLogger(__name__)
 
@@ -14,4 +13,26 @@ def update_handler(bot, received, **kwargs):
     log.debug('Received: %s', received)
 
     save_message(received.message)
-    reply_message(bot, received.message)
+
+    if not hasattr(received.message, 'text'):
+        log.info('Not text message received.')
+        return
+
+    if not should_reply():
+        return
+
+    random_word = get_random_word(received.message)
+    reply_message = get_reply_message(random_word)
+    if not reply_message:
+        log.info('Not reply message')
+
+    reply_type = get_reply_type()
+
+    if reply_type == 'text':
+        reply_text_message(bot, received.message.chat.id ,reply_message)
+
+    if reply_type == 'audio':
+        reply_audio_message(bot, received.message.chat.id, reply_message)
+
+    if reply_type == 'gif':
+        reply_gif_message(bot, received.message.chat.id, random_word)
